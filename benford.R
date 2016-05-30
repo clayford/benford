6752a9b04a1bf4f8652cdb6c40877922bbac77ee
jfccoolbeans <jfc.coolbeans@yahoo.com>
2016-05-30 09:03:23 -0400

    first commit
    add R script

---
+++ benford.R
@@ -0,0 +1,12 @@
+compareBenford <- function(d){
+  digits <- d[!is.na(d)]
+  digits <- substr(stringr::str_extract(as.character(abs(digits)), pattern = "[^0\\.]"),1,1)
+  digits <- factor(digits, levels = 1:9) # ensure all digits represented
+  depth <- prop.table(table(digits))
+  ben <- log10(1 + (1/(1:9)))
+  dat2 <- data.frame(ben, depth)
+  names(dat2) <- c("Benford","Digit",deparse(substitute(d)))
+  dat2L <- reshape2::melt(dat2,id.vars="Digit", variable.name = "Type", value.name = "Frequency")
+  ggplot(dat2L, aes(x=Digit, y=Frequency, fill=Type)) +
+    geom_bar(stat = "identity", position = "dodge")
+}
