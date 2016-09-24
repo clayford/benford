3c8785219fc3e5735c9e34941d4765b554fcec13
Clay Ford <joe.clayton.ford@gmail.com>
2016-09-24 09:40:50 -0400

    add ggplot2 check

--- benford.R
+++ benford.R
@@ -1,4 +1,5 @@
 compareBenford <- function(d){
+  if(!"ggplot2" %in% rownames(installed.packages())) stop("This function requires the ggplot2 package.")
   digits <- d[!is.na(d)]
   digits <- substr(stringr::str_extract(as.character(abs(digits)), pattern = "[^0\\.]"),1,1)
   digits <- factor(digits, levels = 1:9) # ensure all digits represented
@@ -7,6 +8,6 @@ compareBenford <- function(d){
   dat2 <- data.frame(ben, depth)
   names(dat2) <- c("Benford","Digit",deparse(substitute(d)))
   dat2L <- reshape2::melt(dat2,id.vars="Digit", variable.name = "Type", value.name = "Frequency")
-  ggplot(dat2L, aes(x=Digit, y=Frequency, fill=Type)) +
+  ggplot2::ggplot(dat2L, aes(x=Digit, y=Frequency, fill=Type)) +
     geom_bar(stat = "identity", position = "dodge")
 }
